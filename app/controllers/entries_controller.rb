80161ab735596b16210a5fd58a83a0485cac2caa
Lukasz Malyszko <lukas.malyszko@gmail.com>
2025-08-12 22:10:51 +0200

    Enhance entry management: update entries controller for sorting, add edit and update actions, improve UI with turbo frames, and create styles for edit forms

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,13 +1,13 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_entry, only: [:show, :destroy]
+  before_action :set_entry, only: %i[show edit update destroy]
 
   def index
-    @entries = current_user.entries
-    @main_entry = current_user.entries.first
+    @entries = current_user.entries.order(:name)
+    @main_entry = current_user.entries.order(:name).first
   end
 
-  def show;  end
+  def show; end
 
   def new
     @entry = Entry.new
@@ -28,6 +28,20 @@ class EntriesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @entry.update(entry_params)
+      flash.now[:notice] = "<strong>#{@entry.name}</strong> has been updated!".html_safe
+      respond_to do |format|
+        format.html { redirect_to @entry }
+        format.turbo_stream {}
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @entry.destroy
     flash.now[:notice] = "#{@entry.name} has been deleted."
@@ -40,7 +54,7 @@ class EntriesController < ApplicationController
   private
 
   def entry_params
-    params.expect(entry: [:name, :url, :username, :password])
+    params.expect(entry: %i[name url username password])
   end
 
   def set_entry
