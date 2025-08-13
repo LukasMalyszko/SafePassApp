566ce0dc3ad658b853182307a1a37edb6f3af1b6
Lukasz Malyszko <lukas.malyszko@gmail.com>
2025-08-13 10:50:14 +0200

    Refactor entry management: implement search functionality, enhance UI with search form, and update styles for better layout

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,13 +4,22 @@ class Entry < ApplicationRecord
   validates :name, :username, :password, presence: true
   validate :url_must_be_valid
 
-  encrypts  :username, deterministic: true
-  encrypts   :password
+  encrypts :username, deterministic: true
+  encrypts :password
+
+  scope :search_name, lambda { |name|
+    where('entries.name ILIKE ?', "%#{name}%") if name.present?
+  }
+
+  def self.search(name)
+    search_name(name).order(:name)
+  end
 
   private
+
   def url_must_be_valid
-    unless url.include?('http' || 'https')
-      errors.add(:url, "is not valid")
-    end
+    return if url.include?('http' || 'https')
+
+    errors.add(:url, 'is not valid')
   end
 end
